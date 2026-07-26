e8b52ddb05f6f8244230cc042ba102b8bb1628e8
sebastian <44591891+cbxss@users.noreply.github.com>
2026-07-26 13:13:53 -0700

    refactor: separate browser state types

--- clients/go/page.go
+++ clients/go/page.go
@@ -13,25 +13,6 @@ type Page struct {
 	b  *Browser
 }
 
-type SessionCookie struct {
-	Name     string   `json:"name"`
-	Value    string   `json:"value"`
-	Domain   string   `json:"domain"`
-	Path     string   `json:"path"`
-	Secure   bool     `json:"secure"`
-	HTTPOnly bool     `json:"http_only"`
-	SameSite *string  `json:"same_site"`
-	Expires  *float64 `json:"expires"`
-}
-
-type BrowserState struct {
-	Cookies        []SessionCookie   `json:"cookies"`
-	LocalStorage   map[string]string `json:"localStorage"`
-	SessionStorage map[string]string `json:"sessionStorage"`
-	UserAgent      string            `json:"userAgent"`
-	URL            string            `json:"url"`
-}
-
 func (p *Page) ID() string { return p.id }
 
 func (p *Page) call(ctx context.Context, method string, params map[string]any, result any) error {
