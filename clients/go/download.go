3dcad2344bad0eb1681d242963fa7721f0967e23
cbxss <44591891+cbxss@users.noreply.github.com>
2026-07-26 18:29:44 -0700

    fix: download eoka-server 0.1.2 by default (#22)

--- clients/go/download.go
+++ clients/go/download.go
@@ -13,7 +13,7 @@ import (
 	"strings"
 )
 
-const serverReleaseVersion = "0.1.1"
+const serverReleaseVersion = "0.1.2"
 
 const defaultReleaseBaseURL = "https://github.com/shrimp-software/eoka-tools/releases/download"
 
